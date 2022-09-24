662435d7feeddbbbecc405565b6d9d413ad215c8
tukangfork <102410053+tukangfork@users.noreply.github.com>
2022-09-24 19:44:13 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -94,7 +94,7 @@ YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheReyzu/ReyzuMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheReyzu")
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
