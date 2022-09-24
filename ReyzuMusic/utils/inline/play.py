73f8348a0a13d8f0c93bab38656299ae2a4f3143
tukangfork <102410053+tukangfork@users.noreply.github.com>
2022-09-24 23:09:00 +0700

    Update play.py

--- ReyzuMusic/utils/inline/play.py
+++ ReyzuMusic/utils/inline/play.py
@@ -53,6 +53,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             InlineKeyboardButton(
                 text="⏯",
                 callback_data=f"ADMIN Skip|{chat_id}"
+            ),
             InlineKeyboardButton(
                 text="⏹",
                 callback_data=f"ADMIN Stop|{chat_id}"
