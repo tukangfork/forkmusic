3305755b71fb9fe1868a82ca7e6e554b9fbbbecd
tukangfork <102410053+tukangfork@users.noreply.github.com>
2022-09-28 23:57:25 +0700

    Update play.py

--- ReyzuMusic/utils/inline/play.py
+++ ReyzuMusic/utils/inline/play.py
@@ -110,7 +110,7 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["↺ |◁   II   ▷|   ♡"],
+                text="↺ |◁   II   ▷|   ♡",
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
         ],
