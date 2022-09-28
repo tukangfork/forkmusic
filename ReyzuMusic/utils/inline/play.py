e999b650e54c16f1d9b45638211de570eaf4e336
tukangfork <102410053+tukangfork@users.noreply.github.com>
2022-09-29 01:13:58 +0700

    Update play.py

--- ReyzuMusic/utils/inline/play.py
+++ ReyzuMusic/utils/inline/play.py
@@ -111,7 +111,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                 text=_["Start_8"],
-                callback_data=f"PanelMarkup {videoid}|{chat_id}|{config.MUSIC_BOT_NAME}",
+                callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
         ],
         [
